bd0b5537cc342a33c7ea8e23da2fd4b662e107be
garyyiu2015 <garyyiu2015@gmail.com>
2018-09-10 00:25:27 +0800

    fix the AppIconTheme not flyme style in framework

--- custom_jar.sh
+++ custom_jar.sh
@@ -6,6 +6,12 @@ tempSmaliDir=$2
 moveDirs=("android/app" "android/media")
 
 if [ "$jarBaseName" = "framework" ];then
+    echo ">>> fix the AppIconTheme not flyme style in $jarBaseName"
+    rm -rf $tempSmaliDir/smali/android/app/ApplicationPackageManager*.smali
+	cp -rf ../base/framework.jar.out/smali/android/app/ApplicationPackageManager*.smali $tempSmaliDir/smali/android/app/
+fi
+
+if [ "$jarBaseName" = "framework" ];then
     echo ">>> in custom_jar $jarBaseName"
     for dir_name in "${!moveDirs[@]}"
     do
